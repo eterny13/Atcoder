84ea18b0cff5785ef7343a7c8352baebae7ee876
eterny13 <26991786+eterny13@users.noreply.github.com>
2025-08-25 14:27:17 +0900

    move to tessoku

--- kyopro_tessoku/a17.py
+++ kyopro_tessoku/a17.py
@@ -3,12 +3,9 @@ A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
 dp = [0 for _ in range(n+1)]
-room = [1]
 
-for i in range(1, n):
-  if i == 1:
-    dp[i] = dp[i-1] + A[i-1]
-    continue
+dp[1] = A[0]
+for i in range(2, n):
   dp[i] = min(dp[i-1] + A[i-1], dp[i-2] + B[i-2])
 
 ans = []
@@ -25,4 +22,4 @@ while True:
 
 ans.reverse()
 print(len(ans))
-print(" ".join(map(str, ans)))
+print(*ans)
